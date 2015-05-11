99f218407219292a3e9c2cbfae50162f132e9d3d
Joakim Edlund <joakim.en.edlund@gmail.com>
2015-05-11 21:27:33 +0200

    fixed cd and started on piping

--- main.c
+++ main.c
@@ -13,10 +13,11 @@
 #include "colors.h"
 #include "util.h"
 
-/*Hej jag är en fisk*/
 int terminate();
 void kill_child(pid_t child_id);
 void input_handle(char input[]);
+void cd(const char * input);
+void check_env(const char ** args);
 
 int main(int args, char ** argv) {
 	char cwd[1024], input[81];
@@ -42,11 +43,14 @@ int main(int args, char ** argv) {
 
 void input_handle(char input[]) {
 	pid_t childpid = fork();
+	char * charv[10];
+	split_string(charv, input);
 		
 	if(childpid == 0) { /*Child process*/
-		char * charv[10];
-		split_string(charv, input);
-		execvp(charv[0], charv);
+		if(strcmp(charv[0], "checkEnv") == 0)
+			check_env(charv);
+		else
+			execvp(charv[0], charv);
 		exit(0);
 	} else { 			/*Parent process*/
 		if(childpid == -1) {
@@ -55,6 +59,8 @@ void input_handle(char input[]) {
 			if( ENOMEM == errno ) errormessage = "cannot allocate kernel data";
 			fprintf( stderr, "fork() failed because: %s\n", errormessage );
 		}
+		if(strcmp(charv[0], "cd") == 0)
+			cd(charv[1]);
 		wait(NULL);
 	}
 }
@@ -68,3 +74,33 @@ int terminate() {
 void kill_child(pid_t child_id) {
 	kill(child_id, SIGKILL);
 }
+
+void cd(const char * input) {
+	chdir(input);
+}
+
+void check_env(const char ** args) {
+	int pipe_filedesc[2];
+	pid_t childpid = fork();
+	
+	pipe(pipe_filedesc);
+	if(childpid == 0) {
+		/*Child process*/
+		dup2(pipe_filedesc[1], STDOUT_FILENO);
+		close(pipe_filedesc[0]);
+		close(pipe_filedesc[1]);
+		execlp("printenv", "printenv", (char *) 0);
+	}
+	childpid = fork();
+	if(childpid == 0) {
+		/*Child process*/
+		dup2(pipe_filedesc[0], STDIN_FILENO);
+		close(pipe_filedesc[1]);
+		close(pipe_filedesc[0]);
+		execlp("sort", "sort", NULL);
+	}
+
+}
+
+
+
